240943cbab4744acb42b7684163124ae958ef56b
manel <manelgavalda@iesebre.com>
2016-10-19 18:30:52 +0200

    test

--- src/RomanNumbers.php
+++ src/RomanNumbers.php
@@ -3,11 +3,22 @@
 class RomanNumbers
 {
     public function convert($number){
+
+        $conver = array(
+            1 => "I",
+            5 => "V",
+            10 => "X",
+            50 =>
+        );
+
         if ($number==10){
             return "X";
         }
         if ($number==5){
             return "V";
+        }
+        if ($number==4){
+
         }
         return str_repeat('I',$number);
         }
